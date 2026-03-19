195a06d085b3ffc607964bbf5cfe6aafeda29045
Zach Hamilton <Zach.hamilton@kingsway.school.nz>
2026-03-19 20:01:56 +1300

    Validated and fixed welcome margins and resizing

--- Term 1 2026 DGT Website/scripts/scripts.js
+++ Term 1 2026 DGT Website/scripts/scripts.js
@@ -3,28 +3,28 @@
 // every time there is a click this function runs
 document.addEventListener('click', e =>{
     // returns a boolean depending on weather the target has the attribute data-dropdown-button
-    const isDropdownButton = e.target.matches("[data-dropdown-button]")
+    const isDropdownButton = e.target.matches("[data-dropdown-button]");
     // If we are not clicking on the dropdown button (the thing that opens the dropdown in the first place) but are clicking somwhere else on the dropdown return/do nothing
-    if (!isDropdownButton && e.target.closest('[data-dropdown]') !=null) return
+    if (!isDropdownButton && e.target.closest('[data-dropdown]') !=null) return;
 
 
     // creates the currentDropdown variable
-    let currentDropdown
+    let currentDropdown;
     // if isDropdownButton = true then set currentDropdown to the closest parent/grandparent then
     //  Toggles the active class
     if (isDropdownButton) {
-        currentDropdown = e.target.closest('[data-dropdown]')
-        currentDropdown.classList.toggle('active')
+        currentDropdown = e.target.closest('[data-dropdown]');
+        currentDropdown.classList.toggle('active');
     }
     
 
     // close all dropdowns exept the dropdown that you clicked on. If you havn't clicked on a dropdown then it closes all dropdowns
     document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
-        if (dropdown === currentDropdown) return
-        dropdown.classList.remove('active')
+        if (dropdown === currentDropdown) return;
+        dropdown.classList.remove('active');
     })
 
-})
+});
 
 
 
