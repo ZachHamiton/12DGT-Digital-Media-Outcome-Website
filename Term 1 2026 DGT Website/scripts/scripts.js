4a5a673239019b277aa6024ccf780a7850cd68ce
Zach Hamilton <Zach.hamilton@kingsway.school.nz>
2026-03-24 17:58:50 +1300

    Fixed spelling most spelling mistakes

--- Term 1 2026 DGT Website/scripts/scripts.js
+++ Term 1 2026 DGT Website/scripts/scripts.js
@@ -2,9 +2,9 @@
 
 // every time there is a click this function runs
 document.addEventListener('click', e =>{
-    // returns a boolean depending on weather the target has the attribute data-dropdown-button
+    // returns a boolean depending on whether the target has the attribute data-dropdown-button
     const isDropdownButton = e.target.matches("[data-dropdown-button]");
-    // If we are not clicking on the dropdown button (the thing that opens the dropdown in the first place) but are clicking somwhere else on the dropdown return/do nothing
+    // If we are not clicking on the dropdown button (the thing that opens the dropdown in the first place) but are clicking somewhere else on the dropdown return/do nothing
     if (!isDropdownButton && e.target.closest('[data-dropdown]') !=null) return;
 
 
@@ -18,26 +18,10 @@ document.addEventListener('click', e =>{
     }
     
 
-    // close all dropdowns exept the dropdown that you clicked on. If you havn't clicked on a dropdown then it closes all dropdowns
+    // close all dropdowns except the dropdown that you clicked on. If you haven't clicked on a dropdown then it closes all dropdowns
     document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
         if (dropdown === currentDropdown) return;
         dropdown.classList.remove('active');
     })
 
 });
-
-
-
-
-// //Parallax
-
-
-// // gets the paralax_titile id and stores it in a variable
-// const parallax = document.getElementById("paralax_title");
-
-
-// // everytime you scroll the function is run
-// window.addEventListener("scroll" , function(){
-//     // gets offset and times it by 0.7 so that it scrolls slower then the rest of the page. 
-//     let offset = this.window.pageYOffset;
-//     parallax.style.backgroundPositionY = offset * 0.7 + "px"})
